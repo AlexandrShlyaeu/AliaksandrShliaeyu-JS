f132257f4613f77761712e0a43f247da7b6543ed
Alexandr <shlyaeu@gmail.com>
2018-05-06 09:08:14 +0300

    webpack + ts + lits+ tests

--- task6/src/components/App.tsx
+++ task6/src/components/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Button from 'antd/lib/button';
+import react from 'react';
+import button from 'antd/lib/button';
 import 'antd/lib/button/style/css';
 interface AppProps {
-    message: string,
+  message: string,
 };
 export default function({ message }: AppProps ) {
-    return (
-        <div>
-            <h1>Hello {message}</h1>
-            <Button type="primary">Test</Button>
-        </div>
-    );
+  return (
+    <div>
+        <h1>Hello {message}</h1>
+        <button type="primary">Test</button>
+    </div>
+  );
 };
